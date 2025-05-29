2fc2ec0c96627d7dcb918c22d60702343ab6d580
Viderspace <viderspace@gmail.com>
2025-05-29 13:44:38 +0300

    Pdf reader fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-import pdfjsWorker from 'vite-plugin-pdfjs-worker';
-
 // https://vite.dev/config/
 export default defineConfig({
   base: '/TeacherPageTest/',
-  plugins: [react(), pdfjsWorker()],
+  plugins: [react()],
 })
