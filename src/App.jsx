0d7c6935a584ab3a656ece9a5b396a039690c2b9
Viderspace <viderspace@gmail.com>
2025-05-29 12:21:42 +0300

    delpoy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-
+// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 function App() {
   const [messages, setMessages] = useState([
     { role: "system", 
